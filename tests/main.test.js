c704e498b02a539c6bfb09a0071beab6238e37db
AlessandraEsser78 <alessandra.esser.114734@a.fecaf.com.br>
2026-01-04 18:56:48 -0300

    Adiciona testes unitários para calcularTotal usando Jest

---
+++ tests/main.test.js
@@ -0,0 +1,11 @@
+const { calcularTotal } = require('../src/main');
+
+test('Calcula total com desconto', () => {
+    const pedidos = [{valor:100}, {valor:50}];
+    expect(calcularTotal(pedidos, 0.1)).toBe(135);
+});
+
+test('Calcula total sem desconto', () => {
+    const pedidos = [{valor:200}, {valor:50}];
+    expect(calcularTotal(pedidos, 0)).toBe(250);
+});
